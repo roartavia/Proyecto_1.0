c454f46bca052d59288fb25e5973ed2c765b7b50
viksabo <vsaborio4@gmail.com>
2016-01-05 10:58:06 -0600

    Se incluye un nuevo controlador y su vista corresponiete a Instituciones

---
+++ Proyecto_1.0/Controllers/InstitucionesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_1._0.Models;
+
+namespace Proyecto_1._0.Controllers
+{
+    public class InstitucionesController : Controller
+    {
+        private ConexionDb db = new ConexionDb();
+
+        // GET: Instituciones
+        public ActionResult Index()
+        {
+            return View(db.instituciones.ToList());
+        }
+
+        // GET: Instituciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instituciones instituciones = db.instituciones.Find(id);
+            if (instituciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instituciones);
+        }
+
+        // GET: Instituciones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Instituciones/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_institucion,nombre_institucion")] Instituciones instituciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.instituciones.Add(instituciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(instituciones);
+        }
+
+        // GET: Instituciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instituciones instituciones = db.instituciones.Find(id);
+            if (instituciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instituciones);
+        }
+
+        // POST: Instituciones/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_institucion,nombre_institucion")] Instituciones instituciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instituciones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(instituciones);
+        }
+
+        // GET: Instituciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instituciones instituciones = db.instituciones.Find(id);
+            if (instituciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instituciones);
+        }
+
+        // POST: Instituciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Instituciones instituciones = db.instituciones.Find(id);
+            db.instituciones.Remove(instituciones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
