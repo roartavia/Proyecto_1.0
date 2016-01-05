15975e73590466d04dd53980cc3ef0f7170fa360
roartavia <fofillo01@gmail.com>
2016-01-05 11:52:38 -0600

    commit view

---
+++ Proyecto_1.0/Controllers/ViewDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_1._0.Controllers
+{
+    public class ViewDataController : Controller
+    {
+        // GET: ViewData
+        public ActionResult ViewServices()
+        {
+            return View();
+        }
+    }
+}
