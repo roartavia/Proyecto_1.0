dcc4f9ee5924a988e7de859a1e3894db5ad98fe7
pbaltodanom <pablobama14@gmail.com>
2016-01-09 10:47:14 -0600

    MostrarRequisitoTramiteDato controlador

---
+++ Proyecto_1.0/Controllers/MostrarRequisitoTramiteDatoController.cs
@@ -0,0 +1,42 @@
+﻿using Proyecto_1._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace Proyecto_1._0.Controllers
+{
+    public class MostrarRequisitoTramiteDatoController : Controller
+    {
+
+        [HttpPost]
+        // GET: MostrarRequisitoTramiteDato
+        public ActionResult Index(Proyecto_1._0.Models.MostrarRequisitoTramiteDato model)
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection();
+            mySqlConnection.ConnectionString = "server=localhost;user id=pablo;password=141093;database =pintae;persistsecurityinfo=True";
+            MySqlCommand cmd = new MySqlCommand("mostrar_requisito_tramite_dato", mySqlConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new MySqlParameter("@tramite", MySqlDbType.VarChar)).Value = model.IDTramite;
+
+            if (mySqlConnection.State != ConnectionState.Open)
+            {
+                mySqlConnection.Open();
+
+                MySqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                DataTable dt = new DataTable();
+                dt.Load(rdr);
+            }
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
