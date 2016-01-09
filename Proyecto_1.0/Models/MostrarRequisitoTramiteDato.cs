eab8b3de4c4ece981cb6eca99549068c37fb7aa6
pbaltodanom <pablobama14@gmail.com>
2016-01-09 10:46:53 -0600

    MostrarRequisitoTramiteDato modelo

---
+++ Proyecto_1.0/Models/MostrarRequisitoTramiteDato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_1._0.Models
+{
+    public class MostrarRequisitoTramiteDato
+    {
+        [Required(ErrorMessage = "*")]
+        public string IDTramite
+        {
+            get;
+            set;
+        }
+    }
+}
